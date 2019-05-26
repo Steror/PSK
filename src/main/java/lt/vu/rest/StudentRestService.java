e66a8133162fb6858e4421dee99aaef3fad3462e
Steror <tomas.kizielas@gmail.com>
2019-05-26 17:02:41 +0300

    REST Get, Post, Put

--- src/main/java/lt/vu/rest/StudentRestService.java
+++ src/main/java/lt/vu/rest/StudentRestService.java
@@ -1,15 +1,16 @@
 package lt.vu.rest;
 
+import lombok.Getter;
 import lt.vu.entities.Student;
+import lt.vu.usecases.cdi.dao.StudentDAO;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @ApplicationScoped
 @Path("/student")
@@ -18,9 +19,45 @@ public class StudentRestService {
 
     @Inject private EntityManager em;
 
+    @Inject
+    private StudentDAO studentDAO;
+
+    @Getter
+    private Student student = new Student();
+
     @GET
     @Path("/{studentId}")
-    public Student find(@PathParam("studentId") Integer studentId) {
-        return em.find(Student.class, studentId);
+    public Response find(@PathParam("studentId") Integer studentId) {
+        return Response.status(200).entity(em.find(Student.class, studentId)).build();
+    }
+
+    @POST
+    @Path("/{firstName}/{lastName}/{regNo}")
+    @Transactional
+    public Response add(@PathParam("firstName") String firstName,
+                       @PathParam("lastName") String lastName,
+                       @PathParam("regNo") String regNo) {
+        student.setFirstName(firstName);
+        student.setLastName(lastName);
+        student.setRegistrationNo(regNo);
+        studentDAO.create(student);
+        return Response.status(201).build();
+    }
+
+    @PUT
+    @Path("/{studentId}/{firstName}/{lastName}/{regNo}")
+    @Transactional
+    public Response update(@PathParam("studentId") Integer studentId,
+                          @PathParam("firstName") String firstName,
+                          @PathParam("lastName") String lastName,
+                          @PathParam("regNo") String regNo) {
+        student = em.find(Student.class, studentId);
+        if (student == null)
+            return Response.status(204).build();
+        student.setFirstName(firstName);
+        student.setLastName(lastName);
+        student.setRegistrationNo(regNo);
+        studentDAO.updateAndFlush(student);
+        return Response.status(200).build();
     }
 }
